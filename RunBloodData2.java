ac1dfe4ca651f19ef51f5a9322bcd10727562798
Darkov0221 <cardinales.360568@pasay-edsa.sti.edu.ph>
2024-10-11 15:14:02 +0800

    RunBloodData2.java
    04 Hands-on Activity 1 Continuation

--- RunBloodData2.java
+++ RunBloodData2.java
@@ -1,1 +1,56 @@
+import java.util.Scanner;
 
+class BloodData2 {
+    private String bloodType;
+    private String rhFactor;
+    
+    public BloodData2() {
+        this.bloodType = "O";
+        this.rhFactor = "+";
+    }
+    
+    public void setBloodType(String bloodType) {
+        this.bloodType = bloodType;
+    }
+    
+    public void setRhFactor(String rhFactor) {
+        this.rhFactor = rhFactor;
+    }
+    
+    public String getBloodType() {
+        return this.bloodType;
+    }
+    
+    public String getRhFactor() {
+        return this.rhFactor;
+    }
+    
+    public void display() {
+        System.out.println(getBloodType() + getRhFactor() + " is added to the blood bank.");
+    }
+}
+
+public class RunBloodData2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        BloodData2 bd = new BloodData2();
+        
+        System.out.print("Enter blood type of patient: ");
+        String bloodTypeInput = sc.nextLine();
+        
+        System.out.print("Enter the Rhesus factor (+ or -): ");
+        String rhFactorInput = sc.nextLine();
+        
+        if (!bloodTypeInput.isEmpty()) {
+            bd.setBloodType(bloodTypeInput);
+        }
+        if (!rhFactorInput.isEmpty()) {
+            bd.setRhFactor(rhFactorInput);
+        }
+        
+        bd.display();
+        
+        sc.close();
+    }
+}
